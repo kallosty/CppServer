4ee8bd1ade7686cb368616f6101b264b593e4702
kallosty <cnuastro2014@gmail.com>
2025-05-02 15:44:44 +0900

    Thread_Spin //.vcxproj- UserManager와 AccountManager를 삭제해서 오류 발생한것을 해당 파일에서 include 되어있는 것 삭제

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -5,9 +5,36 @@
 #include <atomic>
 #include <mutex>
 
+//Spin Lock
 
+mutex m;
+int32 sum = 0;
+
+void Add()
+{
+    for (int32 i = 0; i < 100'000; i++)
+    {
+        lock_guard<mutex> gaurd(m);
+        sum++;
+    }
+}
+
+void Sub()
+{
+    for (int32 i = 0; i < 100'000; i++)
+    {
+        lock_guard<mutex> gaurd(m);
+        sum--;
+    }
+}
 
 int main()
 {
-    
+    thread t1(Add);
+    thread t2(Sub);
+
+    t1.join();
+    t2.join();
+
+    cout << sum << endl;
 }
