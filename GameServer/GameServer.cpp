6e6314cf20b92258e1edb26d349da08c3d50f97b
kallosty <cnuastro2014@gmail.com>
2025-05-05 21:04:43 +0900

    Memory_Update

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -1,38 +1,8 @@
 ﻿#include "pch.h"
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-#include <iostream>
-#include "CorePch.h"
-#include <thread>
-#include <atomic>
-#include <mutex>
-#include <windows.h>
-#include <chrono>
-#include <future>
-#include "ConcurrentQueue.h"
-#include "ConcurrentStack.h"
-
-<<<<<<< HEAD
-class TestLock
-=======
 
 #include "RefCounting.h"
 
 class Wraight : public RefCountable
->>>>>>> Stashed changes
-=======
-
-#include "RefCounting.h"
-
-class Wraight : public RefCountable
->>>>>>> Stashed changes
-=======
-
-#include "RefCounting.h"
-
-class Wraight : public RefCountable
->>>>>>> Stashed changes
 {
 public:
     int _hp = 150;
@@ -54,27 +24,6 @@ public:
 
     /*void Test(WraightRef& target)
     {
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-        int32 value = testLock.TestRead();
-        cout << value << endl;
-        this_thread::sleep_for(1ms);
-=======
-LockQueue<int32> q;
-LockFreeStack<int32> s;
-
-void Push()
-{
-    while (true)
-    {
-        int32 value = rand() % 100;
-        s.Push(value);
-
-        //this_thread::sleep_for(100ms);
-=======
-=======
->>>>>>> Stashed changes
 
     }*/
 
@@ -95,46 +44,6 @@ void Push()
         }
 
         return false;
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-    }
-
-    WraightRef _target = nullptr;
-};
-
-using MissileRef = TSharedPtr<Missile>;
-<<<<<<< Updated upstream
-
-void Pop()
-{
-    while (true) {
-        auto data = s.TryPop();
-        if (data != nullptr) cout << *data << endl;
->>>>>>> parent of 36864ab (Thread_ThreadManager)
-=======
-
-    }*/
-
-    bool Update()
-    {
-        if (_target == nullptr)
-            return true;
-
-        int posX = _target->_posX;
-        int posY = _target->_posY;
-
-        //TODO : 쫓아간다.
-        if (_target->_hp == 0)
-        {
-            _target->ReleaseRef();
-            _target = nullptr;
-            return true;
-        }
-
-        return false;
->>>>>>> Stashed changes
     }
 
     WraightRef _target = nullptr;
@@ -144,33 +53,6 @@ using MissileRef = TSharedPtr<Missile>;
 
 int main()
 {
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< HEAD
-    for (int32 i = 0; i < 2; i++)
-    {
-        GThreadManager->Launch(ThreadWrite);
-    }
-
-    for (int32 i = 0; i < 5; i++)
-    {
-        GThreadManager->Launch(ThreadRead);
-    }
-=======
-    shared_ptr<int32> ptr;
-    bool value = atomic_is_lock_free(&ptr);
->>>>>>> parent of 36864ab (Thread_ThreadManager)
-
-    thread t1(Push);
-    thread t2(Pop);
-    thread t3(Pop);
-
-
-
-    t1.join();
-    t2.join();
-    t3.join();
-=======
     WraightRef wraight(new Wraight());
     wraight->ReleaseRef();
     MissileRef missile(new Missile());
@@ -183,37 +65,6 @@ int main()
     //wraight->ReleaseRef();
     wraight = nullptr;
 
-=======
-    WraightRef wraight(new Wraight());
-    wraight->ReleaseRef();
-    MissileRef missile(new Missile());
-    missile->ReleaseRef();
-
-    missile->SetTarget(wraight);
-    // 레이스가 피격 당함
-    wraight->_hp = 0;
-    //delete wraight;
-    //wraight->ReleaseRef();
-    wraight = nullptr;
-
->>>>>>> Stashed changes
-=======
-
-int main()
-{
-    WraightRef wraight(new Wraight());
-    wraight->ReleaseRef();
-    MissileRef missile(new Missile());
-    missile->ReleaseRef();
-
-    missile->SetTarget(wraight);
-    // 레이스가 피격 당함
-    wraight->_hp = 0;
-    //delete wraight;
-    //wraight->ReleaseRef();
-    wraight = nullptr;
-
->>>>>>> Stashed changes
     while (true)
     {
         if (missile)
@@ -229,11 +80,4 @@ int main()
     //delete missile;
     //missile->ReleaseRef();
     missile = nullptr;
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 }
