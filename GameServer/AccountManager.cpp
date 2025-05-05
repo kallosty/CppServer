4570ea4dd10e37ac1ebfe5dea8d674351766b5fe
kallosty <cnuastro2014@gmail.com>
2025-05-05 20:26:27 +0900

    ?

---
+++ GameServer/AccountManager.cpp
@@ -0,0 +1,19 @@
+#include "pch.h"
+#include "AccountManager.h"
+#include "PlayerManager.h"
+
+AccountManager GAccountManager;
+
+void AccountManager::AccountThenPlayer()
+{
+	WRITE_LOCK;
+
+	this_thread::sleep_for(1s);
+
+	GPlayerManager.Lock();
+}
+
+void AccountManager::Lock()
+{
+	WRITE_LOCK;
+}
---
+++ GameServer/PlayerManager.h
@@ -0,0 +1,11 @@
+#pragma once
+class PlayerManager
+{
+	USE_LOCK;
+
+public:
+	void PlayerThenAcoount();
+	void Lock();
+};
+
+extern PlayerManager GPlayerManager;
